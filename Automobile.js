e6d8cd98fe224482f282b6c0c370ffcac3de8272
dom111 <dom111@users.noreply.github.com>
2022-02-25 20:35:08 +0000

    Initial commit.

---
+++ Automobile.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Automobile = void 0;
+const Advance_1 = require("@civ-clone/core-science/Advance");
+class Automobile extends Advance_1.default {
+}
+exports.Automobile = Automobile;
+exports.default = Automobile;
+//# sourceMappingURL=Automobile.js.map
